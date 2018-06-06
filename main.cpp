67f2c6c403469c3bae88eee46d1ed393bc2fcdbb
lcarranco <lcarranco@gmail.com>
2018-06-06 09:22:25 -0500

    Extract and set grades

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    int values;
+    string line;
+    vector<int> grades;
+
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        ss >> values;
+        grades.push_back(values);
+    }
+    for (vector<int>::iterator it = grades.begin(); it != grades.end(); ++it) {
+        cout << *it << " ";
+    }
+
     return 0;
 }
